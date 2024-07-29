0696bf912cb933094ae925a0deedfc8d105d6e06
unozer <40232537+unozer@users.noreply.github.com>
2024-07-29 16:05:01 +0200

    First Commit

---
+++ src/main/java/it/gp/springsec2/Springsec2Application.java
@@ -0,0 +1,13 @@
+package it.gp.springsec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsec2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsec2Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/gp/springsec2/Springsec2ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gp.springsec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsec2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
